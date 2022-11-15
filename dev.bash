55016e4877a07db4d1ce2972ec02b58dc4ce4c79
Joshua Mühlfort <muehlfort@gonicus.de>
2022-11-15 09:01:01 +0000

    Fix URL in dev.bash
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- dev.bash
+++ dev.bash
@@ -16,5 +16,5 @@ echo 'Example curl API call:'
 cat <<EOF
 curl -XPOST -H "Content-Type: application/json" \
      -d '{"title": "Some hypervisors are broken", "impactType": {"slug": "performance_degration"}, "components": [{"slug": "hypervisor-00003"}, {"slug": "hypervisor-00004"}], "phase": {"slug": "working_on_it"}}' \
-     127.0.0.1:5000/incidents
+     127.0.0.1:5000/api/incidents
 EOF

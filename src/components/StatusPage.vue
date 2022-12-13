a0a8f1ebd4ff6dac2b84e7ef3f1eec2a74ba9424
Joshua Mühlfort <muehlfort@gonicus.de>
2022-12-13 12:37:37 +0000

    Basic time frame support
    Signed-off-by: Joshua Mühlfort <muehlfort@gonicus.de>

--- src/components/StatusPage.vue
+++ src/components/StatusPage.vue
@@ -14,7 +14,6 @@
             <v-container>
                 <v-row>
                     <v-col>
-                        <div class="text-subtitle-1 mb-2">Components</div>
                         <v-expansion-panels>
                             <v-expansion-panel v-for="component in components" :key="component.slug">
                                 <v-expansion-panel-title
@@ -36,12 +35,24 @@
                             </v-expansion-panel>
                         </v-expansion-panels>
                     </v-col>
+                </v-row>
+                <v-row>
                     <v-col>
-                        <div class="text-subtitle-1 mb-2">Incidents</div>
-                        <v-timeline side="end">
+                        <v-timeline side="end" align="start">
+                            <v-timeline-item
+                                :icon="'mdi-chevron-triple-up'"
+                                icon-color="white"
+                                size="small">
+                                since {{startTimeString}}
+                                <v-btn @click="loadEarlier">
+                                    Load more
+                                </v-btn>
+                            </v-timeline-item>
                             <v-timeline-item
                                 v-for="incident in incidents" :key="incident.ID"
-                                :dot-color="incident.phase.slug === 'closed' ? 'green' : 'red'">
+                                :dot-color="incident.phase.slug === 'closed' ? 'green' : 'red'"
+                                :icon="'mdi-fire'"
+                                size="large">
                                 <v-card>
                                     <v-card-title :class="['text-h6', incident.phase.slug === 'closed' ? 'bg-green' : 'bg-red']">
                                         {{incident.title}}
@@ -95,6 +106,15 @@
                                     </v-card-text>
                                 </v-card>
                             </v-timeline-item>
+                            <v-timeline-item
+                                size="small"
+                                icon="mdi-chevron-triple-down"
+                                icon-color="white">
+                                up to {{endTimeString}}
+                                <v-btn @click="loadLater">
+                                    Load more
+                                </v-btn>
+                            </v-timeline-item>
                         </v-timeline>
                     </v-col>
                 </v-row>
@@ -108,26 +128,48 @@ import axios from 'axios'
 
 export default {
     data() {
+        let now = new Date()
+        now.setHours(0)
+        now.setMinutes(0)
+        now.setSeconds(0)
         return {
             components: [],
-            incidents: []
+            incidents: [],
+            timeRange: {
+                start: new Date(new Date(now).setDate(now.getDate() - 7)),
+                end: now
+            }
         }
     },
     mounted() {
         this.sync()
         setInterval(this.sync, 1000)
     },
+    computed: {
+        startTimeString() {
+            return this.timeRange.start.toLocaleDateString()
+        },
+        endTimeString () {
+            return this.timeRange.end.toLocaleDateString()
+        }
+    },
     methods: {
-        sync: function(e) {
+        sync: function() {
             let self = this
             axios.get(`https://${window.location.host}/api/components`)
             .then(function(response){
                 self.components = response.data
             })
-            axios.get(`https://${window.location.host}/api/incidents`)
+            axios.get(`https://${window.location.host}/api/incidents?start=${this.timeRange.start.toISOString()}&end=${this.timeRange.end.toISOString()}`)
             .then(function(response){
                 self.incidents = response.data
             })
+        },
+        loadEarlier: function() {
+            this.timeRange.start = new Date(this.timeRange.start.setDate(this.timeRange.start.getDate() - 7))
+        },
+        loadLater: function() {
+            this.timeRange.end = new Date(this.timeRange.end.setDate(this.timeRange.end.getDate() + 7))
         }
     }
 }

19c785e0c33c19290fa30e2c98c560c544b4fc18
Sebastian Kaupe <kaupe@gonicus.de>
2023-12-29 14:04:48 +0000

    replace old content with the new HTML prototype in preparation of actual development.
    Signed-off-by: Sebastian Kaupe <kaupe@gonicus.de>

---
+++ html-prototype/statuspage.js
@@ -0,0 +1,65 @@
+
+const CB_COLORBLIND = "colorBlindMode";
+const CB_INCREASED_TEXT = "increasedTextSizeMode";
+
+let themeColors = {}
+
+let TextSizes = {
+    Default: "",
+    Increased: ""
+}
+
+function init() {
+    // Retrieve the colors ordinarily used so that we can switch back to them
+    // when leaving colorblind mode.
+    let r = document.querySelector(":root");
+    let styles = getComputedStyle(r);
+    let colorNames = Array.from(styles).filter(name => name.startsWith("--") && name.endsWith("-color"));
+    colorNames.forEach(name => themeColors[name] = styles.getPropertyValue(name));
+    // Get text size info
+    TextSizes.Default = styles.getPropertyValue("--default-text-size");
+    TextSizes.Increased = styles.getPropertyValue("--increased-text-size");
+    // Establish state of accessibility options
+    if (document.cookie.match("AccessibilityColorblind=true")) {
+        document.getElementById(CB_COLORBLIND).checked = true;
+        switchColorMode();
+    }
+    if (document.cookie.match("AccessibilityIncreasedText=true")) {
+        document.getElementById(CB_INCREASED_TEXT).checked = true;
+        switchTextSize();
+    }
+}
+
+function switchColorMode() {
+    let checkbox = document.getElementById(CB_COLORBLIND);
+    if (checkbox.checked == true) {
+        let r = document.querySelector(":root");
+        r.style.setProperty("--maintenance-color", "#E1BE6A");
+        r.style.setProperty("--fine-color", "#8CE05D");
+        r.style.setProperty("--limited-color", "#5D3A9B");
+        r.style.setProperty("--broken-color", "#D62C13");
+        document.cookie = "AccessibilityColorblind=true";
+        // Switch to colors for colorblind
+    } else {
+        // Re-establish ordinary colors
+        let r = document.querySelector(":root");
+        r.style.setProperty("--maintenance-color", themeColors["--maintenance-color"]);
+        r.style.setProperty("--fine-color", themeColors["--fine-color"]);
+        r.style.setProperty("--limited-color", themeColors["--limited-color"]);
+        r.style.setProperty("--broken-color", themeColors["--broken-color"]);
+        document.cookie = "AccessibilityColorblind=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+}
+
+function switchTextSize() {
+    let checkbox = document.getElementById(CB_INCREASED_TEXT);
+    if (checkbox.checked) {
+        let r = document.querySelector(":root");
+        r.style.setProperty("--default-text-size", TextSizes.Increased);
+        document.cookie = "AccessibilityIncreasedText=true";
+    } else {
+        let r = document.querySelector(":root");
+        r.style.setProperty("--default-text-size", TextSizes.Default);
+        document.cookie = "AccessibilityIncreasedText=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+}

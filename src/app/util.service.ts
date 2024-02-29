68a1a0ddf23169c2e4fd7cc25febbb314ac14e96
Sebastian Kaupe <kaupe@gonicus.de>
2024-02-29 15:16:07 +0000

    switch date format to use timezone names
    Signed-off-by: Sebastian Kaupe <kaupe@gonicus.de>

--- src/app/util.service.ts
+++ src/app/util.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AppConfigService } from './app-config.service';
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import timezone from 'dayjs/plugin/timezone'
+import advformat from 'dayjs/plugin/advancedFormat'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,10 @@ export class UtilService {
 
   constructor(
     private config: AppConfigService
-  ) {}
+  ) {
+    dayjs.extend(timezone);
+    dayjs.extend(advformat);
+  }
 
   formatDate(dt: Dayjs | null): string {
     if (!dt) {

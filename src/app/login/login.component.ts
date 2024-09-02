50a79e659fe7200a5dcbb3cd829e0e3285676bf3
Sebastian Kaupe <kaupe@gonicus.de>
2024-09-02 07:41:29 +0000

    fix missing auto-redirect
    Signed-off-by: Sebastian Kaupe <kaupe@gonicus.de>

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -26,5 +26,9 @@ export class LoginComponent {
     let login = await firstValueFrom(this.security.checkAuth());
     this.waiting = false;
     this.success = login.isAuthenticated;
+    if (this.success) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      this.router.navigate([""]);
+    }
   }
 }
